685ba43e89b81bce06d861d9310591e9d324f761
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-08-28 21:04:04 -0700

    Updates

---
+++ precache-manifest.40a0200c23300be1201b2f37357ea49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6b42e892238156f1cf9153f907b986",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a7c84efdedd5f6948e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "20b911a702f3849e8f91",
+    "url": "/static/js/2.f95f0bf1.chunk.js"
+  },
+  {
+    "revision": "59a7c84efdedd5f6948e",
+    "url": "/static/js/main.52219477.chunk.js"
+  },
+  {
+    "revision": "d8d54d7af465afa7a99b",
+    "url": "/static/js/runtime~main.f75d2e85.js"
+  }
+]);
